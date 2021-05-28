be2d29c0b75f69db916b1c805c1de53fa11d9319
Saint Wish <SaintWish@users.noreply.github.com>
2021-05-28 04:12:19 -0600

    Revert "force run workflows"
    This reverts commit 23b321b012aaa384c6f9e84a69c89d14b25095f1.

--- apps/opus/entrypoint.sh
+++ apps/opus/entrypoint.sh
@@ -9,6 +9,5 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-
 # Run the Server
 eval ${MODIFIED_STARTUP}
